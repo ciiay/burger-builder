9f7a60b44230d59689d5eb10fe1666209afcb11c
ciiay <redrainyday@gmail.com>
2021-01-04 22:50:14 -0500

    Added More, Less button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,8 +10,17 @@ const controls = [
 ];
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>
+            Current Price: <strong>{props.price.toFixed(2)}</strong>
+        </p>
         {controls.map((ctrl) => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+                key={ctrl.label}
+                label={ctrl.label}
+            />
         ))}
     </div>
 );
