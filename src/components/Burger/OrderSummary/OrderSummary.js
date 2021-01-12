dd4688ac3badbc426fd7a8aa0573efa8ca6a5250
ciiay <redrainyday@gmail.com>
2021-01-11 23:13:24 -0500

    Improved the performance by adding shouldComponentUpdate to Modal class

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,41 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 import PropTypes from 'prop-types';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-        return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-                {props.ingredients[igKey]}
-            </li>
-        );
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingrdients:</p>
-            <ul>{ingredientSummary}</ul>
-            <p>
-                <strong>Total Price: ${props.price.toFixed(2)}</strong>
-            </p>
-            <p>Countinue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>
-                CANCEL
-            </Button>
-            <Button btnType='Success' clicked={props.purchaseCountinued}>
-                COUNTINUE
-            </Button>
-        </Aux>
-    );
-};
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a class
+    componentDidUpdate() {
+        console.log('[OrderSummary] updated!');
+    }
 
-orderSummary.propTypes = {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(
+            (igKey) => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>
+                            {igKey}
+                        </span>
+                        : {this.props.ingredients[igKey]}
+                    </li>
+                );
+            }
+        );
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingrdients:</p>
+                <ul>{ingredientSummary}</ul>
+                <p>
+                    <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+                </p>
+                <p>Countinue to Checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>
+                    CANCEL
+                </Button>
+                <Button
+                    btnType='Success'
+                    clicked={this.props.purchaseCountinued}
+                >
+                    COUNTINUE
+                </Button>
+            </Aux>
+        );
+    }
+}
+
+OrderSummary.propTypes = {
     ingredients: PropTypes.object,
     price: PropTypes.number,
     purchaseCanceled: PropTypes.func,
     purchaseCountinued: PropTypes.func,
 };
 
-export default orderSummary;
+export default OrderSummary;
